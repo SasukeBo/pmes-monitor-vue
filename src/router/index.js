272de3cb129dde564fec984f8fb7f865176eaeff
SasukeBo <809754210@qq.com>
2020-09-04 19:13:08 +0800

    config

--- src/router/index.js
+++ src/router/index.js
@@ -142,6 +142,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
+  base: '/monitor/',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: '/monitor',
-  baseUrl: '/monitor'
+  publicPath: '/monitor'
 }
