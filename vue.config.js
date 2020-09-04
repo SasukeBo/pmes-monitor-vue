06b5511a841e6701d894d3b79a4f2e0cc5e61dd9
SasukeBo <809754210@qq.com>
2020-09-04 19:50:02 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,16 @@ module.exports = {
       entry: 'src/main.js',
       filename: 'index.html'
     }
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .use('url-loader')
+      .loader('file-loader')
+      .tap((options) => {
+        options.name = 'img/monitor/[name].[ext]'
+        return options
+      })
   }
 }
